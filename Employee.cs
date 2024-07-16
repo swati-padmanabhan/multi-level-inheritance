d5ae05220abbb9bbf66037a5d4fcb97d86dbbcb1
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-16 22:03:23 +0530

    initial commit of both files

---
+++ Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilevelInheritence.models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Constructor 
+        //It is the Base Class
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public string GetEmployeeData()
+        {
+            return $"Employee ID: {Id} \nEmployee Name: {Name}";
+        }
+    }
+
+    //Intermediate Class which inherits properties from Employee Class
+    public class Department : Employee
+    {
+        public string DepartmentName { get; set; }
+        public Department(int id, string name, string departmentName) : base(id, name)
+        {
+            DepartmentName = departmentName;
+        }
+        public string GetDepartmentData()
+        {
+            return $"Department Name: {DepartmentName}";
+        }
+    }
+
+    //Derived Class which inherits properties from Department Class
+    public class Project : Department
+    {
+        public string ProjectName { get; set; }
+
+        public Project(int id, string name, string departmentName, string projectName)
+            : base(id, name, departmentName)
+        {
+            ProjectName = projectName;
+        }
+        public string  GetProjectData()
+        {
+            return $"Project Name: {ProjectName}";
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using MultilevelInheritence.models;
+
+namespace MultilevelInheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Project project = new Project(101, "Ram", "SCM", "Logistics");
+
+            //Display employee data
+            Console.WriteLine(project.GetEmployeeData());
+
+            //Display department data
+            Console.WriteLine(project.GetDepartmentData());
+
+            //Display project information
+            Console.WriteLine(project.GetProjectData());      
+        }
+    }
+}
